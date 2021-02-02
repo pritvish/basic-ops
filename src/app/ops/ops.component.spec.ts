f440736cf794c2ac53f9817e4a93b74b192ae37c
pritvish <pritvish.mandal80@gmail.com>
2021-02-02 23:44:56 +0530

    first commit for basic-ops

---
+++ src/app/ops/ops.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OpsComponent } from './ops.component';
+
+describe('OpsComponent', () => {
+  let component: OpsComponent;
+  let fixture: ComponentFixture<OpsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ OpsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(OpsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
