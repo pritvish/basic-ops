1a2e3de9f8615beee5592fa51cd6b1c5654e73b3
pritvish <pritvish.mandal80@gmail.com>
2021-02-03 22:59:01 +0530

    binding operations commit

--- src/app/ops/ops.component.ts
+++ src/app/ops/ops.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-ops',
-  templateUrl: './ops.component.html',
-  styleUrls: ['./ops.component.css']
+    selector: 'app-ops',
+    templateUrl: './ops.component.html',
+    styleUrls: ['./ops.component.css']
 })
 export class OpsComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+    public interpolationEx = "interpolationEx text";
+
+    //property binding : sending value from ts file to view
+    // value from ts file is always bound to dom object of view but not the attribute of html although it may appear as is
+    public propertybind = "propertybind text";
+    public isDisabled = true;
+
+    //event binding
+    count = 0
+
+    increment() {
+        return this.count++;
+    }
+
+    decrement() {
+        return this.count--;
+    }
+
+    // two way binding
+
+    twoWayBind = ""
 
 }
